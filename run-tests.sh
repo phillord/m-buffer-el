ce6dfb0023d1766a2616cdccb60ecdb53e4f2637
Phillip Lord <phillip.lord@newcastle.ac.uk>
2014-02-17 17:43:22 +0000

    Use Emacs var

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,4 @@
 ## run in subdir because test files there there
-emacs -batch -l ./test/dash.el  -l m-buffer.el -l ./test/m-buffer-test.el \
+EMACS=${EMACS-emacs}
+$EMACS -batch -l ./test/dash.el  -l m-buffer.el -l ./test/m-buffer-test.el \
     -f ert-run-tests-batch-and-exit
