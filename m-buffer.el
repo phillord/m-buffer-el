94251c2abe41380022b1fcf195a749aeaa51b26a
Phillip Lord <phillip.lord@newcastle.ac.uk>
2014-02-23 19:17:47 +0000

    Package metadata added.

--- m-buffer.el
+++ m-buffer.el
@@ -5,6 +5,7 @@
 ;; Author: Phillip Lord <phillip.lord@newcastle.ac.uk>
 ;; Maintainer: Phillip Lord <phillip.lord@newcastle.ac.uk>
 ;; Version: 0.2-SNAPSHOT
+;; Package-Requires: ((dash "2.5.0")(emacs "24.3"))
 
 ;; The contents of this file are subject to the GPL License, Version 3.0.
 ;;
