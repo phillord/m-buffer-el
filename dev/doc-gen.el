3f71fb0b62ff3ff387869486bd7bc49c34650c07
Stefan Monnier <monnier@iro.umontreal.ca>
2022-10-19 15:27:16 -0400

    * dev/doc-gen.el: Fix up copyright check.
    Also, use `lexical-binding`, and let it compile even if `lentic-doc` is absent.

--- dev/doc-gen.el
+++ dev/doc-gen.el
@@ -1,10 +1,14 @@
+;; -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Free Software Foundation, Inc.
+
 (require 'load-relative)
 
 (require-relative "../m-buffer-macro")
 (require-relative "../m-buffer")
 (require-relative "../m-buffer-at")
 
-(require 'lentic-doc)
+(require 'lentic-doc nil t)
 
 (defun doc-gen ()
   (lentic-doc-htmlify-package 'm-buffer))
